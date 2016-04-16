03771eb285404d36b274db5ef1489e14753e9a9f
Janek <janek.ilgner@kingart-games.com>
2016-04-16 19:59:32 +0200

    Cards and Hand Visualization WIP

---
+++ Assets/Scripts/Cards/CardComponent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets;
+
+public class CardComponent : MonoBehaviour, IClickable
+{
+
+    public Animator animator;
+
+    private bool _selected;
+    public bool Selected
+    {
+        get { return _selected; }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClick()
+    {
+        if (!Selected)
+        {
+            animator.SetTrigger("Select");
+            _selected = true;
+        }
+        else
+        {
+            animator.SetTrigger("Deselect");
+            _selected = false;
+        }
+        
+
+    }
+}
