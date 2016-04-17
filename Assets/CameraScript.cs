46c5adf56b6328f0c296703bd854213beb351ae2
thomas-burger <burger_thomas@web.de>
2016-04-17 22:02:59 +0200

    - added CameraScript.cs

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    public float ScrollSpeed = 100.0f;
+    public float MoveSpeed = 20.0f;
+	// Update is called once per frame
+	void Update ()
+    {
+        float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (mouseScroll != 0)
+        {
+            Camera.main.transform.position += ScrollSpeed * mouseScroll * Camera.main.transform.forward * Time.deltaTime;
+        }
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Camera.main.transform.position += MoveSpeed*(new Vector3(moveX, 0, moveZ))*Time.deltaTime;
+	}
+}
