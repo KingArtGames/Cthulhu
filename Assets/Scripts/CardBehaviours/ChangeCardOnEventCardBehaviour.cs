2f0e3f9dd3a0e598a0bc634a5cfc8016ecfc5d6b
thomas-burger <burger_thomas@web.de>
2016-04-17 16:27:22 +0200

    - added ChangeCardOnEventCardBehaviour.cs, KillEnemyCardBehaviour.cs and SanityCheckCardBehaviour.cs - added some cards - added purple tokens - changed Ghoul and DamageSpell card

---
+++ Assets/Scripts/CardBehaviours/ChangeCardOnEventCardBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.CardBehaviours
+{
+    class ChangeCardOnEventCardBehaviour : AbstractCardBehaviour
+    {
+        public GameObject changeToCard;
+        public CardLifecycleStep executeStep;
+        public Field.DeckLocation executeLocation;
+
+        [Inject]
+        public Field fieldOfPayne;
+
+        private BaseCard _card;
+
+        public override void Initialize(BaseCard owner)
+        {
+            owner.RegisterLivecycleStepExecutor(executeStep, OnEvent);
+            _card = owner;
+        }
+
+        private CardOperation OnEvent(Field.DeckLocation loc)
+        {
+            if (executeLocation == loc)
+            {
+                CardOperation result = new CardOperation();
+                StartCoroutine(ChangeCard(result));
+                return result;
+            }
+
+            return CardOperation.DoneSuccess;
+        }
+
+        private IEnumerator ChangeCard(CardOperation op)
+        {
+
+            //play animation / SFX
+
+            //
+            _card.Replace(changeToCard);
+
+            op.Complete(CardOperation.Result.Success);
+            yield break;
+        }
+
+        public override string GetDescription()
+        {
+            string descriptionString = "[" + executeStep.ToString() + " in " + executeLocation.ToString() + "]: Change to " + changeToCard.name;
+
+            return descriptionString;
+        }
+    }
+}
---
+++ Assets/Scripts/CardBehaviours/SanityCheckCardBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+
+namespace Assets.Scripts.CardBehaviours
+{
+    class SanityCheckCardBehaviour : AbstractCardBehaviour
+    {
+        public CompareType compareType;
+        public int compareValue;
+        public GameObject changeToCard;
+
+        [Inject]
+        public TokenService tokenService;
+
+        private BaseCard _card;
+
+        public override void Initialize(BaseCard owner)
+        {
+            _card = owner;
+            //owner.RegisterLivecycleStepExecutor(executeStep, OnEvent);
+           
+        }
+
+        private CardOperation OnSanityChanged(Field.DeckLocation loc)
+        {
+            CardOperation result = new CardOperation();
+            StartCoroutine(SanityCheck(result));
+            return result;
+        }
+
+        private IEnumerator SanityCheck(CardOperation op)
+        {
+
+            //play animation / SFX
+
+            //
+            if(Check(tokenService.GetTokenStack(TokenService.TokenType.sanity).Count.Value, compareType, compareValue))
+            {
+                _card.Replace(changeToCard);
+            }
+            else
+            {
+                op.Complete(CardOperation.Result.Failure);
+            }
+
+            op.Complete(CardOperation.Result.Success);
+            yield break;
+        }
+
+        public override string GetDescription()
+        {
+            string descriptionString = "[Sanity" + ToString(compareType) + compareValue + "]: SanityCheck";
+
+            return descriptionString;
+        }
+    }
+}
