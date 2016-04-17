534677e93e4384306cfac92dd42ab53dc741a193
thomas.burger <burger_thomas@web.de>
2016-04-17 12:47:14 +0200

    - added CreateTokenOnPlayCardBehaviour - added CreateTokenOnStartTurnCardBehaviour

---
+++ Assets/Scripts/CardBehaviours/CreateTokenOnPlayCardBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.CardBehaviours
+{
+    class CreateTokenOnPlayCardBehaviour : AbstractCardBehaviour
+    {
+        public TokenService.TokenType tokenType;
+        public int numTokens = 0;
+        public List<Field.DeckLocation> executeLocations = new List<Field.DeckLocation>();
+
+        [Inject]
+        public Field fieldOfPayne;
+        [Inject]
+        public TokenService tokenService;
+
+        public override void Initialize(BaseCard owner)
+        {
+            owner.RegisterLivecycleStepExecutor(CardLifecycleStep.Add, OnAdd);
+        }
+
+        private CardOperation OnAdd(Field.DeckLocation loc)
+        {
+            if (executeLocations.Contains(loc))
+            {
+                CardOperation result = new CardOperation();
+                StartCoroutine(AddTokens(result, loc));
+                return result;
+            }
+
+            return CardOperation.DoneSuccess;
+        }
+
+        private IEnumerator AddTokens(CardOperation op, Field.DeckLocation loc)
+        {
+            
+            //play animation / SFX
+
+            //
+            tokenService.AddTokens(tokenType, numTokens);
+
+            op.Complete(CardOperation.Result.Success);
+            yield break;
+        }
+    }
+}
