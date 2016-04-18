bc26a42eed31f691d163d028ba2bd58dca3a129a
Janek <janek.ilgner@kingart-games.com>
2016-04-19 01:38:20 +0200

    final gui

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -20,10 +20,12 @@ namespace Assets.Scripts.UI
 
         public Text HealthText;
         public Text SanityText;
-        public Text DoomText;
+        //public Text DoomText;
         public Text BlueTokensText;
         public Text GreenTokensText;
         public Text PurpleTokensText;
+
+        public GameObject TutorialImage;
 
         public Text PhaseText;
 
@@ -39,12 +41,17 @@ namespace Assets.Scripts.UI
         {
             _healthChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.health).Count.Subscribe(_ => RefreshHealth());
             _sanityChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.sanity).Count.Subscribe(_ => RefreshSanity());
-            _doomChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.doom).Count.Subscribe(_ => RefreshDoom());
+            //_doomChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.doom).Count.Subscribe(_ => RefreshDoom());
             _blueTokensChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.blue).Count.Subscribe(_ => RefrehsBlueTokens());
             _greenTokensChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.green).Count.Subscribe(_ => RefreshGreenTokens());
             _purpleTokensChangedSubscription = TokenService.GetTokenStack(TokenService.TokenType.purple).Count.Subscribe(_ => RefreshPurpleTokens());
 
             Processor.AddGamePhaseChangedListener(PhaseChanged);
+        }
+
+        public void OnTutorialButtonClick()
+        {
+            TutorialImage.SetActive(!TutorialImage.activeSelf);
         }
 
         private void PhaseChanged()
@@ -74,7 +81,7 @@ namespace Assets.Scripts.UI
         }
         private void RefreshDoom()
         {
-            DoomText.text = "Doom: " + TokenService.Tokens[TokenService.TokenType.doom].Count;
+            //DoomText.text = "Doom: " + TokenService.Tokens[TokenService.TokenType.doom].Count;
         }
         private void RefrehsBlueTokens()
         {
